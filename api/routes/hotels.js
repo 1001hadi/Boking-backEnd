bc5dcc9e8eb85acaa013f9f8b5bcef9342ccf500
1001hadi <hadi.jaf.1001@gmail.com>
2024-01-29 07:58:30 -0700

    node/mongo CRUD operatin has been created and tested with postman

--- api/routes/hotels.js
+++ api/routes/hotels.js
@@ -15,18 +15,46 @@ router.post("/", async (req, res) => {
   }
 });
 // update
-// router.put("/:id", async (req, res) => {
-//   try {
-//     const updatedHotel = await Hotel.findByIdAndUpdate(req.params.id, {
-//       set: req.body,
-//     });
-//     res.status(200).json(updatedHotel);
-//   } catch (err) {
-//     res.status(500).json(err);
-//   }
-// });
+router.put("/:id", async (req, res) => {
+  try {
+    const updatedHotel = await Hotel.findByIdAndUpdate(
+      req.params.id,
+      {
+        $set: req.body,
+      },
+      { new: true }
+    );
+    res.status(200).json(updatedHotel);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
 // delete
+router.delete("/:id", async (req, res) => {
+  try {
+    await Hotel.findByIdAndDelete(req.params.id);
+    res.status(200).json("Hotel has been deleted");
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
 // get
+router.get("/:id", async (req, res) => {
+  try {
+    const hotel = await Hotel.findById(req.params.id);
+    res.status(200).json(hotel);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
 // getAll
+router.get("/", async (req, res) => {
+  try {
+    const hotel = await Hotel.find(req.params.id);
+    res.status(200).json(hotel);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
 
 export default router;
