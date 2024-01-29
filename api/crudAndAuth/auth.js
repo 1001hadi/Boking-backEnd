c0d0e433c7aadf2e8f9dbd6809e3e418c4bc93a5
1001hadi <hadi.jaf.1001@gmail.com>
2024-01-29 13:08:15 -0700

    CRUD opration hass been completed

--- api/crudAndAuth/auth.js
+++ api/crudAndAuth/auth.js
@@ -1,6 +1,7 @@
 import User from "../models/User.js";
 import bcrypt from "bcryptjs";
 import jwt from "jsonwebtoken";
+import { createError } from "../handle/error.js";
 
 export const register = async (req, res, next) => {
   try {
--- api/routes/rooms.js
+++ api/routes/rooms.js
@@ -1,5 +1,23 @@
 import express from "express";
+import {
+  createRoom,
+  deleteRoom,
+  getAllRooms,
+  getRoom,
+  updateRoom,
+} from "../crudAndAuth/room.js";
+import { verifyAdmin } from "../handle/verifyToken.js";
 
 const router = express.Router();
+
+router.post("/:hotelid", verifyAdmin, createRoom);
+
+router.put("/:id", verifyAdmin, updateRoom);
+
+router.delete("/:id/:hotelid", verifyAdmin, deleteRoom);
+
+router.get("/:id", getRoom);
+
+router.get("/", getAllRooms);
 
 export default router;
